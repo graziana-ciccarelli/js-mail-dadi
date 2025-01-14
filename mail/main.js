c579100c1561c056b1afd3ff52ab177b14abfa57
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-14 17:11:50 +0100

    aggiunta ciclo più stampa finale del messaggio (esercizio mail)

--- mail/main.js
+++ mail/main.js
@@ -10,3 +10,25 @@ const invitati = [
 //Chiedere l'email all'utente
 
 const emailUtente = prompt("Inserisci la tua email");
+
+//Variabile per verificare se la email è stata trovata
+
+let emailTrovata = false;
+
+//Controllare se l'email è nella lista degli invitati
+
+for (let i = 0; i < invitati.length; i++){
+    if (invitati[i] === emailUtente){
+        emailTrovata = true;
+    }
+}
+
+
+//Stampa un messaggio appropriato
+
+if (emailTrovata){
+    console.log("Sei stato invitato!");
+   
+} else {
+    console.log("Mi dispiace, non sei nella lista degli invitati.");
+}
