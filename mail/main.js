ee49f1f3199f2d381219e67a058eb606b4f11326
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-14 16:10:22 +0100

    aggiunte lista email(invitati) e richiesta email dell'utente

---
+++ mail/main.js
@@ -0,0 +1,12 @@
+//Lista email invitati
+
+const invitati = [
+    "marco.rossi@gmail.com",
+    "mario.verdi@gmail.com",
+    "chiara.bianchi@gmail.com",
+    "sara.neri@gmail.com"
+];
+
+//Chiedere l'email all'utente
+
+const emailUtente = prompt("Inserisci la tua email");
