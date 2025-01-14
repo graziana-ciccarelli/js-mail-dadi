1fbd5f2e428d0d9d108cd2c40185d24cc2c5905a
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-14 15:35:37 +0100

    aggiunta visualizzazine punteggi (esercizio dadi)

--- dadi/main.js
+++ dadi/main.js
@@ -5,3 +5,8 @@ let punteggioGiocatore = Math.floor(Math.random() * 6) + 1
 //Generiamo il punteggio del computer (numero random tra 1 e 6)
 
 let punteggioComputer =  Math.floor(Math.random() * 6) + 1
+
+
+//Visualizzazione dei punteggi
+console.log("Punteggio del giocatore: " + punteggioGiocatore);
+console.log("Punteggio del computer: " + punteggioComputer);
